20305f43bb76e168383d6c04af5eb3f4f94a21ca
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-20 23:59:27 +0530

    Day-6 Logical Program

---
+++ PrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProblems
+{
+    public class PrimeNumber
+    {
+        public static void CheckPrimeOrNot()
+        {
+            Console.WriteLine("Please enter a number");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+
+
+            for (int i = 2; i <= N; i++)
+            {
+                int count = 0;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        count = 1;
+                        break;
+
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.WriteLine(i + " is not prime number");
+                }
+                else
+                {
+                    Console.WriteLine(i + " is prime number");
+                }
+            }
+        }
+    }
+}
