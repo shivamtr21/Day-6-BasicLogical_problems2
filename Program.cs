f39be8a7078e3c2436e1022885a2f01c960ef8f8
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-20 23:58:02 +0530

    Day-6 Logical Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicLogicalProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FibonacciSeries.FindFibonacciSeries();
+            // ReverseNumber.NumberReverse();
+            //PerfectNumber.FindPerfectNumber();
+            //PrimeNumber.CheckPrimeOrNot();
+            CupanNumber.Couponnumbers();
+        }
+    }
+}
