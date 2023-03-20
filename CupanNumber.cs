46f84e129d3021ca1e28fb6d53e88ba45a4c86c4
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-21 00:03:34 +0530

    Day-6 Logical Program

---
+++ CupanNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProblems
+{
+    public class CupanNumber
+    {
+        public static void Couponnumbers()
+        {
+            Console.WriteLine("Enter the length of the coupon number:");
+            int length = int.Parse(Console.ReadLine());
+
+            string couponNumber = GenerateCouponNumber(length);
+
+            Console.WriteLine("Coupon number: " + couponNumber);
+
+        }
+
+        static string GenerateCouponNumber(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
+        }
+    }
+}
