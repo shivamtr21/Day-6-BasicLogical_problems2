f379076309346403f9ab26a389d0b6b216c6f54f
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-21 00:01:41 +0530

    Day-6 Logical Program

---
+++ ReverseNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProblems
+{
+    public class ReverseNumber
+    {
+        public static void NumberReverse()
+        {
+            int rem;
+            int reverse = 0;
+             
+
+            Console.WriteLine("Enter your number");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            
+             while(N != 0)
+            {
+                rem = N % 10;
+
+
+                reverse = reverse * 10 + rem;
+                N = N / 10;
+
+               
+
+            }
+             
+
+             
+            Console.WriteLine("Reverse num = " + reverse);
+        }
+    }
+}
